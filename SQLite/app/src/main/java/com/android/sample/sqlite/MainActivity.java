f9f8bd18fde1f278e2055548a9294ef82f90edde
ysera34 <ycyoon34@gmail.com>
2017-07-04 10:01:20 +0900

    sqlite test, apply to stetho

--- SQLite/app/src/main/java/com/android/sample/sqlite/MainActivity.java
+++ SQLite/app/src/main/java/com/android/sample/sqlite/MainActivity.java
@@ -1,7 +1,12 @@
 package com.android.sample.sqlite;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.android.sample.sqlite.model.Category;
+import com.android.sample.sqlite.model.CategoryStorage;
+import com.facebook.stetho.Stetho;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (BuildConfig.DEBUG) {
+            Context context = getApplicationContext();
+            Stetho.initializeWithDefaults(this);
+        }
+
+        CategoryStorage categoryStorage = CategoryStorage.get(getApplicationContext());
+        for (int i = 0; i < 5; i++) {
+            Category category = new Category();
+            category.setId(i);
+            category.setNumber(i);
+            category.setName("category name : " + i);
+            categoryStorage.addCategory(category);
+        }
     }
 }
